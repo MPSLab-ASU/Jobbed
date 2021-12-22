e43a2696a9eb948f7e08674da766d8971bd4d296
Christian Cunningham <cc@localhost>
2021-12-21 20:22:40 -0800

    Moved IRQ inlines

--- src/cpu/irq.h
+++ src/cpu/irq.h
@@ -1,6 +1,19 @@
 #ifndef CPU_IRQ_H
 #define CPU_IRQ_H
 
+static inline void enableirq() {
+	asm volatile("cpsie i");
+}
+static inline void disableirq() {
+	asm volatile("cpsid i");
+}
+static inline void enablefiq() {
+	asm volatile("cpsie f");
+}
+static inline void disablefiq() {
+	asm volatile("cpsid f");
+}
+
 void c_irq_handler(void);
 
 #endif
