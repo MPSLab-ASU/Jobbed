969b39a6970d3c4f5ddb89048699325c96939d32
Christian Cunningham <cc@localhost>
2021-12-21 19:28:07 -0800

    Added Debug Output Pseudo Functions

--- src/drivers/uart.a.h
+++ src/drivers/uart.a.h
@@ -1,6 +1,9 @@
 #ifndef DRIVERS_UART_A_H
 #define DRIVERS_UART_A_H
 
+#define PRINTS_DBG(X) uart_char('[');uart_string(__FILE__);uart_char(':');uart_10(__LINE__);uart_string("] ");uart_string(X);
+#define PRINTI_DBG(X) uart_char('[');uart_string(__FILE__);uart_char(':');uart_10(__LINE__);uart_string("] ");uart_10(X);
+
 extern void uart_char(unsigned char c);
 extern void uart_hex(unsigned long data);
 extern void uart_string(char* message);
