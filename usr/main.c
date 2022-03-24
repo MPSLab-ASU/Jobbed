22e4f3def524d787316b40f46e66ec0455d1ed52
Christian Cunningham <cc@localhost>
2022-03-24 09:52:43 -0700

    Seconds delimiter

--- usr/main.c
+++ usr/main.c
@@ -5,7 +5,7 @@ char* ulong_to_string(unsigned long value, char* data)
 {
 	unsigned long t = value;
 	unsigned long c;
-	char* dptr = data + 9;
+	char* dptr = data + 10;
 	for (int i = 0; i <= 10; i++) {
 		c = t%10;
 		*dptr = 0x30 + (c&0xF);
@@ -13,6 +13,10 @@ char* ulong_to_string(unsigned long value, char* data)
 		if (t==0)
 			break;
 		dptr -= 1;
+		if (i == 5) {
+			*dptr = '.';
+			dptr -= 1;
+		}
 	}
 	return dptr;
 }
