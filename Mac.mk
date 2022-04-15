f9804563de127bd3fd65992f1e31b6912d04b4d3
Christian Cunningham <cc@localhost>
2022-04-15 16:31:14 -0700

    Demarcate Make Rule

--- Mac.mk
+++ Mac.mk
@@ -2,6 +2,7 @@ include Common.mk
 QEMU = qemu-system-arm
 CROSS = ./toolchain/gcc-arm-none-eabi-8-2019-q3-update/bin/arm-none-eabi
 
+.PHONY: init
 init:
 	@-mkdir toolchain
 	@curl -o toolchain/mac.chain.tar.bz2 "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/8-2019q3/RC1.1/gcc-arm-none-eabi-8-2019-q3-update-mac.tar.bz2"
