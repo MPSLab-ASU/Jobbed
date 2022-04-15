6b89b4c23927688f718b73f4bb1e05f7c7cbb245
Christian Cunningham <cc@localhost>
2022-04-15 16:24:07 -0700

    Suppress all tar output

--- Colab.mk
+++ Colab.mk
@@ -5,4 +5,4 @@ CROSS = ./toolchain/gcc-arm-none-eabi-8-2019-q3-update/bin/arm-none-eabi
 init:
 	@-mkdir toolchain
 	@curl -o toolchain/colab.chain.tar.bz2 "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/8-2019q3/RC1.1/gcc-arm-none-eabi-8-2019-q3-update-linux.tar.bz2"
-	@tar xjvf toolchain/colab.chain.tar.bz2 -C toolchain/
+	@tar xjf toolchain/colab.chain.tar.bz2 -C toolchain/
--- Mac.mk
+++ Mac.mk
@@ -5,5 +5,5 @@ CROSS = ./toolchain/gcc-arm-none-eabi-8-2019-q3-update/bin/arm-none-eabi
 init:
 	@-mkdir toolchain
 	@curl -o toolchain/mac.chain.tar.bz2 "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/8-2019q3/RC1.1/gcc-arm-none-eabi-8-2019-q3-update-mac.tar.bz2"
-	@tar xjvf toolchain/mac.chain.tar.bz2 -C toolchain/
+	@tar xjf toolchain/mac.chain.tar.bz2 -C toolchain/
 
